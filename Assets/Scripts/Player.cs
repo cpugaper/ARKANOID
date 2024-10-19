be90d7dc4f0929bcd09d2bcbe038058d2b0d1370
Carla Puga <84739899+cpugaper@users.noreply.github.com>
2024-10-19 16:22:20 +0200

    player & ball movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rigidBody2D;
+
+    private float inputValue;
+
+    public float moveSpeed = 25;
+
+    private Vector2 direction; 
+
+
+    void Update()
+    {
+        inputValue = Input.GetAxisRaw("Horizontal");
+
+        if (inputValue == 1)
+        {
+            direction = Vector2.right;
+        }
+        else if (inputValue == -1)
+        {
+            direction = Vector2.left;
+        }
+        else
+        {
+            direction = Vector2.zero; 
+        }
+
+        rigidBody2D.AddForce(direction * moveSpeed * Time.deltaTime * 100);
+
+    }
+}
